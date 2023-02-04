9921ccd26ee7a1bbb76f8344fdf356d835d098ef
king-obobo <oboboebuka1@gmail.com>
2023-02-04 03:16:29 -0800

    Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is a rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    Creates an empty rectangle class
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Repo for the 0x08-python-more_classes ALX project

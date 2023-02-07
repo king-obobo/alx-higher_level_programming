c22babb1d47fd0281981c37eb3598f7ef81e2345
king-obobo <oboboebuka1@gmail.com>
2023-02-07 08:26:02 -0800

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""A read_file function"""
+"""This module defines a read_file function"""
 
 
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout"""
+    """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")

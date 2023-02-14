3d204e6d6d60cc1858d88d12613dd53081a24986
king-obobo <oboboebuka1@gmail.com>
2023-02-14 03:02:23 -0800

    Fixed to pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,10 +17,8 @@ class Base:
         Args:
             id(int): The identity of the new Base
         """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-

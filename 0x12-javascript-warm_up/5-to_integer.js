81562a11880290cc2629e85298bd908cf0d1e0a2
king-obobo <oboboebuka1@gmail.com>
2023-04-12 04:56:32 -0700

    Task 5. An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) {
+if (isNaN(process.argv[2] || process.argv[2] === undefined)) {
   console.log('Not a number');
 } else {
   console.log('My Number: ' + parseInt(process.argv[2]));

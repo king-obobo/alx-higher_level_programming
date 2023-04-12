efc37c6cc7e9fce37e039b10022be75e090066b3
king-obobo <oboboebuka1@gmail.com>
2023-04-12 05:25:33 -0700

    Task 7. I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+let i = 0;
+if (isNaN(arg) || arg === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  while (i < arg) {
+    console.log('C is fun');
+    i++
+  }
+}

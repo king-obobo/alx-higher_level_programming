ffb17f1c0492df29e71d96b2d793b15894b7bca9
king-obobo <oboboebuka1@gmail.com>
2023-04-13 02:51:13 -0700

    Semistandard fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ function factorial (n) {
   if (isNaN(n) || n === 0) {
     return (1);
   }
-  return (n * factorial(n -1));
+  return (n * factorial(n - 1));
 }
 
 console.log(factorial(Number(process.argv[2])));

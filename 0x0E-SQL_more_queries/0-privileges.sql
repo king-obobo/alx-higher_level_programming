4dffb4665283a5f1f8eaebd275aa157beca7d238
king-obobo <oboboebuka1@gmail.com>
2023-04-21 13:44:24 -0700

    Task 0. My privileges!

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- lists all privileges of the MySQL users on your server
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_21'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+Repo containing tasks for ALX 0x0E. SQL - More queries

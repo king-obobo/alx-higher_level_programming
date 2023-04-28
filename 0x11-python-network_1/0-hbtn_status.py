608e37fa8a71dfd84db28551fa32810ca8037e20
king-obobo <oboboebuka1@gmail.com>
2023-04-28 07:14:38 -0700

    Task 0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,7 @@ a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
+
 def fetch_url(web_page):
     with urllib.request.urlopen(web_page) as res:
         content = res.read()
@@ -11,6 +12,7 @@ def fetch_url(web_page):
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
 
 if __name__ == "__main__":
     fetch_url("https://alx-intranet.hbtn.io/status")

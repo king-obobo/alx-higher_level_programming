619eb13ed7560581589e8f20bf12c88f890d0940
king-obobo <oboboebuka1@gmail.com>
2023-04-28 10:16:56 -0700

    Task 7. Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script:
+    - takes in a URL
+    - sends a request to the URL and
+    - displays the body of the response (decoded in utf-8).
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)

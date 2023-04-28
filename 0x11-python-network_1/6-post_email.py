dd62b074a7b0ea963647e5093d86c269b037da5a
king-obobo <oboboebuka1@gmail.com>
2023-04-28 09:50:59 -0700

    Task 6. POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script:
+    - takes in a URL and an email
+    - sends a POST request to the URL and
+    - displays the body of the response (decoded in utf-8)
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {"email": sys.argv[2]}
+
+    r = requests.post(url, params=values)
+    print(r.text)

e74debde4493499bbdfd2d5820e97896d19f5df8
king-obobo <oboboebuka1@gmail.com>
2023-04-28 07:34:33 -0700

    Task 1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,9 +8,9 @@ import sys
 import urllib.request
 
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     url = sys.argv[1]
 
     request = urllib.request.Request(url)
-    with urllib.request.openurl(request) as res:
-        print(dict(response.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(request) as res:
+        print(dict(res.headers).get("X-Request-Id"))
